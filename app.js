a60a0113a990061e7c5d6646c62cde8d1e8e3362
arielmusa <arielmusainfo@gmail.com>
2025-06-23 01:37:35 +0200

    feat(app.js: import teams and players routers

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 import express from "express";
 import "dotenv/config";
+import { db } from "./config/db.js";
 import cors from "cors";
+import { teamsRouter } from "./routers/teamsRouter.js";
+import { playersRouter } from "./routers/playersRouter.js";
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+
+app.use("/teams", teamsRouter);
+app.use("/players", playersRouter);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome" });
+});
 
 app.listen(3000, () => console.log(`Listening port 3000`));
